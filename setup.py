1b2011c5c56e7fb0e203daef1618ab7a834e06cf
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2026-03-08 08:12:43 +0100

    refactor toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# Copyright (c) 2025 Regents of University of Michigan
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
-from setuptools import setup
-
-# Configuration is in pyproject.toml
-setup()
